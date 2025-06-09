790afcedcc684bbd895545ebb937362950aa15c5
mehmet ali sakallı <mhmtalisklli@gmail.com>
2025-06-09 18:39:48 +0300

    gateway first commit

---
+++ src/main/java/eba/rich/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package eba.rich.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eba/rich/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eba.rich.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
